9af160c238bd4feef527d3ee2205727c20be40fe
Francis Renzaho <renzahofr2020@gmail.com>
2023-06-04 14:44:19 +1000

    implementing gotoCalendar()

---
+++ ProjectManagementSystem/src/main/java/managementsystempackage/controller/LoginPageController.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package managementsystempackage.controller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import managementsystempackage.App;
+
+/**
+ * FXML Controller class
+ *
+ * @author renza
+ */
+public class LoginPageController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void gotoCalendar(ActionEvent event) throws IOException {
+        //Temporary navigation
+        App.setRoot("view/calendar");
+    }
+    
+}
