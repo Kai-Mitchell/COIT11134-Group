e8d75c5992dc6a66ba8bf489831cbfccb45eb49f
Francis Renzaho <renzahofr2020@gmail.com>
2023-06-05 15:27:17 +1000

    Interfaces manages mouseEvents

---
+++ ProjectManagementSystem/src/main/java/managementsystempackage/model/IEventListener.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package managementsystempackage.model;
+
+/**
+ *
+ * @author renza
+ */
+public interface IEventListener {
+    public void onClickEvent( Events event);
+    public void onClickDelete( Events event);
+
+}
