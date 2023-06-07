c2d16b00f890c222afd08d2cd6958426fde9b47f
Francis Renzaho <renzahofr2020@gmail.com>
2023-06-07 11:34:45 +1000

    first CalendarBox and Controller

---
+++ ProjectManagementSystem/src/main/java/managementsystempackage/controller/CalendarBoxController.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package managementsystempackage.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+/**
+ *
+ * @author renza
+ */
+public class CalendarBoxController {
+    @FXML
+    private Text txtNumber;
+
+    @FXML
+    private VBox vbDisplay;
+}
