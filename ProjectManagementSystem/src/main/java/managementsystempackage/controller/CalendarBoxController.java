420ee93f719d069b13b2b3f4d20c54c19d4aeef7
canda777 <129179288+canda777@users.noreply.github.com>
2023-06-08 22:36:02 +1000

    Update CalendarBoxController.java

--- ProjectManagementSystem/src/main/java/managementsystempackage/controller/CalendarBoxController.java
+++ ProjectManagementSystem/src/main/java/managementsystempackage/controller/CalendarBoxController.java
@@ -59,6 +59,7 @@ public class CalendarBoxController implements Initializable {
                     }
                 });
             }
+            executorService.shutdown();
         }
     }
     
