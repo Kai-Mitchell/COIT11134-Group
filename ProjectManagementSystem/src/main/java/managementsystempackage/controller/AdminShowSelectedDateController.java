3fe4fa0243a94a0c45a095661845154fdc5fd53b
Francis Renzaho <renzahofr2020@gmail.com>
2023-06-09 21:11:28 +1000

    added Task Data Collection & Display

--- ProjectManagementSystem/src/main/java/managementsystempackage/controller/AdminShowSelectedDateController.java
+++ ProjectManagementSystem/src/main/java/managementsystempackage/controller/AdminShowSelectedDateController.java
@@ -19,15 +19,21 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.util.Duration;
 import managementsystempackage.App;
+import managementsystempackage.model.CalendarBox;
 import managementsystempackage.model.Events;
 import managementsystempackage.model.IEventListener;
+import managementsystempackage.model.Task;
 /**
  * FXML Controller class
  *
@@ -36,18 +42,61 @@ import managementsystempackage.model.IEventListener;
 public class AdminShowSelectedDateController implements Initializable {
 
 
+     @FXML
+    private BorderPane TaskBorderPane;
+
     @FXML
-    private Text txtDate;
+    private Button btnAddTask;
+
     @FXML
-    private VBox vbDisplay;
+    private Button btnAddUser;
+
+    @FXML
+    private Button btnDeleteTask1;
+
+    @FXML
+    private DatePicker dpEndDate;
+
+    @FXML
+    private DatePicker dpStartDate;
+
+    @FXML
+    private DatePicker dpTaskDueDate;
+
     @FXML
     private AnchorPane sidePane1;
+
+    @FXML
+    private AnchorPane sidePane2;
+
+    @FXML
+    private VBox taskDisplay;
+
+    @FXML
+    private TextField tfDescription;
+
+    @FXML
+    private TextField tfEventTitle;
+
+    @FXML
+    private Text txtDate;
+
+    @FXML
+    private VBox vbDisplay;
+
+    @FXML
+    private VBox vbDisplayUsers;
     
     private boolean sidePane1IsShowing;
+    private boolean sidePane2IsShowing;
     private ArrayList<Events> eventArray = new ArrayList<>();
     private IEventListener<Events> iEventListener;
+    private IEventListener<Task> iTaskListener;
     private boolean disableAnimation;
+    private boolean disableAnimation1;
     private Events currentEvent;
+    private Task currentTask;
+    private CalendarBox box;
     /**
      * Initializes the controller class.
      */
@@ -55,9 +104,14 @@ public class AdminShowSelectedDateController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
         disableAnimation = false; // Disable user interaction during animation
+        disableAnimation1 = false;
         sidePane1IsShowing = true;
+        sidePane2IsShowing = true;
         sidePane1.setVisible(!sidePane1IsShowing);
+        sidePane2.setVisible(!sidePane2IsShowing);
         showSidePane1();
+        showSidePane2();
+        
     } 
     
      
@@ -65,6 +119,22 @@ public class AdminShowSelectedDateController implements Initializable {
    private void gotoCalendar() throws IOException{
        SceneNavigation.gotoCalendar();
    }
+   @FXML
+   private void gotoPlannedEvent() throws IOException{
+       SceneNavigation.gotoPlannedEvent();
+   }
+   @FXML
+   private void gotoCompletedEvents() throws IOException{
+       SceneNavigation.gotoCompletedEvents();
+   }
+   
+   @FXML
+   private void gotoLoginPage() throws IOException{
+       SceneNavigation.gotoLoginPage();
+   }
+   
+   @FXML
+   private void addUser(){}
     
     private void addItems(){
         ExecutorService executorService = Executors.newCachedThreadPool();//
@@ -76,14 +146,44 @@ public class AdminShowSelectedDateController implements Initializable {
 
                 @Override
                 public void onClickEvent(Events event) {
+                    Task[] tempTaskList = new Task[2];
+                    tempTaskList[0] = new Task("Task-_-", 0, LocalDate.MAX, null);
+                    tempTaskList[1] = new Task("Tasko_o", 0, LocalDate.MAX, null);
+                    
                     getClickedEvent(event);
+                    System.out.println(sidePane1IsShowing);
                     if(currentEvent != null && currentEvent.equals(event) || !sidePane1IsShowing){
                         sidePane1.setVisible(true);
 
                         showSidePane1();
                         
+                        
                     }
-                    System.out.println("SidePane is Showing: "+ sidePane1IsShowing);
+                    
+                    if(sidePane1IsShowing){
+                        
+                                
+                        for(Task task : tempTaskList ){
+                            try{
+                                FXMLLoader loader = new FXMLLoader(App.class.getResource("view/taskCard.fxml")); //selecting fxml file
+                                Pane pane = loader.load(); //loading file
+                                TaskCardController controller = loader.getController();//get fxml controller
+                                controller.setCardData(task, iTaskListener); //send data to controller
+                                
+
+                                taskDisplay.getChildren().add(pane);
+
+                            }
+                            catch(IOException ioe){
+                                ioe.printStackTrace();
+                            }
+                         
+
+                        }       
+                    }else{
+//                        showSidePane2();
+                    }
+                    
                     currentEvent = event;
                     
                 }
@@ -97,20 +197,37 @@ public class AdminShowSelectedDateController implements Initializable {
                 
             };
             
+            
+            iTaskListener = new IEventListener<Task>() {
+            @Override
+            public void onClickEvent(Task task) {
+
+                if(currentTask != null && currentTask.equals(task) || !sidePane2IsShowing){
+
+                    sidePane2.setVisible(true);
+                    showSidePane2();
+
+                }
+                currentTask = task;
+            }
+
+                @Override
+                public void onClickDelete(Task data) {
+                    System.out.println("Task clicked!");
+
+                }
+            };
+            
             for(Events e : eventArray ){
                 final Events event = e;
-                //execute task on a separete thread
-                 executorService.execute(() -> {
                     try{
-                    FXMLLoader loader = new FXMLLoader(App.class.getResource("view/eventCard.fxml")); //selecting fxml file
-                    Pane pane = loader.load(); //loading file
-                    EventCardController controller = loader.getController();//get fxml controller
-                    controller.setCardData(event, iEventListener); //send data to controller
+                        FXMLLoader loader = new FXMLLoader(App.class.getResource("view/eventCard.fxml")); //selecting fxml file
+                        Pane pane = loader.load(); //loading file
+                        EventCardController controller = loader.getController();//get fxml controller
+                        controller.setCardData(event, iEventListener); //send data to controller
 
-                     Platform.runLater(() -> {
 
-                       vbDisplay.getChildren().add(pane);
-                    });
+                        vbDisplay.getChildren().add(pane);
 
                     }
                     catch(IOException ioe){
@@ -118,10 +235,8 @@ public class AdminShowSelectedDateController implements Initializable {
                     }
 
 
-                });
 
             }
-            executorService.shutdown();
         }
         else{
             System.out.println("No data!!");
@@ -138,7 +253,14 @@ public class AdminShowSelectedDateController implements Initializable {
         
         }
     }
+    private void showSidePane2() {
+        if(!disableAnimation1){
+            disableAnimation1 = true;
+            translationAnimation(0.5, sidePane2, 580, sidePane2IsShowing, () -> disableAnimation1 = false);
+            sidePane2IsShowing = !sidePane2IsShowing;
 
+        }
+    }
     private void translationAnimation(double duration, Node node, double width, boolean isOpen, Runnable onFinished) {
         TranslateTransition translateTransition = new TranslateTransition(Duration.seconds(duration), node);
         translateTransition.setByX(isOpen ? width : -width);
@@ -157,8 +279,10 @@ public class AdminShowSelectedDateController implements Initializable {
     }
     
     //Temporary func that represents data
-    public void setData(ArrayList<Events> eventArray){
-            this.eventArray = eventArray;
+    public void setData(CalendarBox box){
+            this.eventArray = box.eventArray;
+            this.box = box;
+            this.txtDate.setText(box.getDate().toString());
             addItems();
 
     }
